074b0c8d23b9dd16eff7e41bd01375d2137f1eed
mcmo <cmcmordie@charter.net>
2015-01-28 03:11:37 +0000

    clean up routes

--- routes.js
+++ routes.js
@@ -1,25 +1,21 @@
 module.exports = function(app){
 
-  app.get('/make_a_deal', function(req, res) {
-    res.render('make_a_deal', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
+  var options = {
+    title: 'Make a Deal | Chris Learns JS',
+    style: 'make_a_deal'
+  };
+  
+  app.get('/day2/make_a_deal', function(req, res) {
+    res.render('day2/make_a_deal', options);
   });
-  app.get('/day2/make_a_deal_choose_1.html', function(req, res) {
-    res.render('day2/make_a_deal_choose_1', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
+
+  app.get('/day2/:color', function(req, res){
+    
+    res.render('day2/' + req.params.color, options);
   });
-  app.get('/day2/make_a_deal_choose_2.html', function(req, res) {
-    res.render('day2/make_a_deal_choose_2', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
-  });
-  app.get('/day2/make_a_deal_choose_3.html', function(req, res) {
-    res.render('day2/make_a_deal_choose_3', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
-  });
-  app.get('/day2/make_a_deal_final_1.html', function(req, res) {
-    res.render('day2/make_a_deal_final_1', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
-  });
-  app.get('/day2/make_a_deal_final_2.html', function(req, res) {
-    res.render('day2/make_a_deal_final_2', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
-  });
-  app.get('/day2/make_a_deal_final_3.html', function(req, res) {
-    res.render('day2/make_a_deal_final_3', {title: 'Make a Deal | Chris Learns JS', style: 'make_a_deal'});
+  
+  app.get('/day2/:final', function(req, res){
+    res.render('day2/' + req.params.final, options);
   });
 
 };
