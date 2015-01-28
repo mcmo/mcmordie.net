9bc21b21a29e25722e15b7b16648d081c3682cfa
mcmo <cmcmordie@charter.net>
2015-01-28 19:12:01 +0000

    remove color name from info box

--- public/js/random_background.js
+++ public/js/random_background.js
@@ -9,32 +9,17 @@ $(document).ready(function(){
     colorName();
   });
  
-  $.getScript('http://chir.ag/projects/ntc/ntc.js', function(){
-    colorName();
-  });
-
   function colorName(){
     
-    var colorArray = $('body').css('background-color').slice(4, -1).split(', ');
-    var r = colorArray[0];
-    var g = colorArray[1];
-    var b = colorArray[2];
+    var rgbColor = $('body').css('background-color').slice(4, -1).split(', ');
+    var r = rgbColor[0];
+    var g = rgbColor[1];
+    var b = rgbColor[2];
 
     var hexColor = toHex(r) + toHex(g) + toHex(b);
-    var n_match  = ntc.name(hexColor);
     
-    n_rgb        = n_match[0]; // This is the RGB value of the closest matching color
-    n_name       = n_match[1]; // This is the text string for the name of the match
-    //n_exactmatch = n_match[2]; // True if exact color match, False if close-match
-
-    //document.getElementById('color.name').innerText = n_name;
-    document.getElementById('color.rgb').innerText = colorArray;
-    document.getElementById('color.hex').innerText = n_rgb;
-//     if (n_exactmatch === false){
-//       document.getElementById('color.exact').innerText = "No";
-//     } else {
-//       document.getElementById('color.exact').innerText = "Yes";
-//     }
+    document.getElementById('color.rgb').innerText = rgbColor;
+    document.getElementById('color.hex').innerText = hexColor;
 
     function toHex(n) {
       n = parseInt(n,10);
@@ -43,4 +28,7 @@ $(document).ready(function(){
       return "0123456789ABCDEF".charAt((n-n%16)/16) + "0123456789ABCDEF".charAt(n%16);
     }    
   }
+  
+  colorName();
+  
 });
